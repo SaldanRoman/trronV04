5f2c3d734ae64353f458dd7317373b0f0a58c3de
Roman Saldan <saldanroman@gmail.com>
2019-09-06 10:24:06 +0300

    fixes

--- js/article.js
+++ js/article.js
@@ -8,23 +8,30 @@ fetch("./jsons/articles.json")
   });
 
 function makeArticle() {
-  const searchableIDOfObject = +window.location.search.slice(1);
-  let article = articleArrObj.articles.filter(
+  let searchableIDOfObject = +window.location.search.slice(1);
+  if (
+    searchableIDOfObject > articleArrObj.articles.length ||
+    isNaN(searchableIDOfObject)
+  ) {
+    searchableIDOfObject = 0;
+  }
+  const articleObj = articleArrObj.articles.filter(
     item => item.id === searchableIDOfObject
   );
+
   const articleContent = document.querySelector(".article-content");
   const image = document.createElement("img");
-  image.setAttribute("src", article[0].image);
-  image.setAttribute("alt", article[0].title);
+  image.setAttribute("src", articleObj[0].image);
+  image.setAttribute("alt", articleObj[0].title);
   image.setAttribute("class", "article-content-image");
 
   const title = document.createElement("p");
   title.setAttribute("class", "article-content-title");
-  title.innerText = article[0].title;
+  title.innerText = articleObj[0].title;
 
   const text = document.createElement("p");
   text.setAttribute("class", "article-content-text");
-  text.innerText = article[0].text;
+  text.innerText = articleObj[0].text;
 
   articleContent.appendChild(image);
   articleContent.appendChild(title);
