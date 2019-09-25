3fe4442dccd205c01660cfc561f882cc87a70498
Roman Saldan <saldanroman@gmail.com>
2019-09-25 23:20:11 +0300

    done

--- js/blog.js
+++ js/blog.js
@@ -1,3 +1,4 @@
+"use strict";
 let arrayOfAllArticles;
 let arrayOfArticles;
 let sumOfPages;
@@ -7,16 +8,16 @@ let sumOfPages;
     if (xhr.readyState === 4 && xhr.status === 200) {
       arrayOfArticles = JSON.parse(xhr.response).articles;
       arrayOfAllArticles = JSON.parse(xhr.response).articles;
-      pages();
-      createContent(arrayOfArticles);
+      createPages();
+      createContent(arrayOfArticles, 0);
     }
   };
   xhr.open("GET", "./jsons/articles.json", true);
   xhr.send();
 })();
 
-function createContent(arrayOfArticles, numOfPage = 0) {
-  let arr = arrayOfArticles.slice(numOfPage, numOfPage + 6);
+function createContent(arrayOfArticles, numOfPage) {
+  let arr = arrayOfArticles.slice(numOfPage, numOfPage + 8);
   if (numOfPage + 6 > arrayOfArticles.length) {
     arr = arrayOfArticles.slice(numOfPage, arrayOfArticles.length);
   }
@@ -29,7 +30,6 @@ function createArticle(articleObj) {
   const blogContent = document.querySelector(".blog-content");
   const article = document.createElement("div");
   article.setAttribute("class", "blog-content-article");
-
   const imgLink = document.createElement("a");
   imgLink.setAttribute("class", "blog-content-article-img-wrapper");
   imgLink.setAttribute("href", "articles.html?" + articleObj.id);
@@ -38,57 +38,50 @@ function createArticle(articleObj) {
   img.setAttribute("src", articleObj.image);
   img.setAttribute("alt", articleObj.title);
   imgLink.appendChild(img);
-
   const text = document.createElement("div");
   text.setAttribute("class", "blog-content-article-text");
   const title = document.createElement("div");
   title.setAttribute("class", "blog-content-article-text-title");
   title.innerText = articleObj.title;
-
   const date = document.createElement("div");
   date.setAttribute("class", "blog-content-article-text-date");
   date.innerText = articleObj.date;
-
   const description = document.createElement("div");
   description.setAttribute("class", "blog-content-article-text-description");
-  description.innerText = articleObj.text.slice(0, 450) + "  . . .";
-
+  description.innerText = articleObj.text.slice(0, 260) + "   ...";
   const link = document.createElement("a");
   link.setAttribute("class", "blog-content-article-text-link");
   link.setAttribute("href", "articles.html?" + articleObj.id);
   link.innerText = "more";
-
   text.appendChild(title);
   text.appendChild(date);
   text.appendChild(description);
   text.appendChild(link);
-
   article.appendChild(imgLink);
   article.appendChild(text);
-
   blogContent.appendChild(article);
 }
 
-function pages() {
+function createPages() {
   let articlescount = 0;
   const pagesOfContent = document.querySelector(".blog-pages");
-  sumOfPages = Math.ceil(arrayOfArticles.length / 6);
-  if (arrayOfArticles.length <= 6) {
+  sumOfPages = Math.ceil(arrayOfArticles.length / 8);
+  if (arrayOfArticles.length <= 8) {
     return;
   }
   for (let pagesindex = 1; pagesindex <= sumOfPages; pagesindex++) {
     const pageIndex = document.createElement("a");
-    pageIndex.setAttribute("href", "#blog");
+    pageIndex.setAttribute("href", "#");
     pageIndex.setAttribute("class", "blog-pages-links");
     pageIndex.setAttribute("data", articlescount);
     pageIndex.innerText = pagesindex;
     pagesOfContent.appendChild(pageIndex);
-    articlescount += 6;
+    articlescount += 8;
   }
 }
 
 //this function add switch pages property on pages links
-setTimeout(function() {
+function switchPages() {
   const pageLinks = document.querySelectorAll(".blog-pages-links");
   pageLinks.forEach(function(item) {
     item.addEventListener("click", function(event) {
@@ -100,4 +93,26 @@ setTimeout(function() {
       createContent(arrayOfArticles, +item.getAttribute("data"));
     });
   });
-}, 500);
+}
+setTimeout(switchPages, 500);
+
+document.querySelectorAll(".blog-links").forEach(function(item) {
+  item.addEventListener("click", function(event) {
+    arrayOfArticles = [];
+    arrayOfArticles = arrayOfAllArticles.filter(function(item) {
+      return item.categorie === event.target.innerText;
+    });
+    document.querySelector(".blog-content").innerHTML = "";
+    createContent(arrayOfArticles, 0);
+    document.querySelector(".blog-pages").innerHTML = "";
+    createPages();
+    switchPages();
+    document.querySelector("h1").innerText = event.target.innerText;
+    document.querySelector(".nav-links-add").innerHTML =
+      "» " +
+      '<a href="#" class="nav-links">' +
+      " " +
+      event.target.innerText +
+      "</a>";
+  });
+});
